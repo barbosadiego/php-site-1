81d86b5d6efd3a9082d54d2a3f2fc4f17e9e57b1
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-11 15:38:03 -0300

    about page about

--- about.php
+++ about.php
@@ -41,6 +41,34 @@
     <h1>about us</h1>
   </div>
 
+  <!-- ABOUT -->
+  <section class="about-page about">
+
+    <div class="image">
+      <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+      <h3>why choose us</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est nemo aliquam sapiente blanditiis illo ducimus cumque, voluptate distinctio iusto magnam rem placeat, vero dicta dolorum sequi! Adipisci, fuga. Molestias, at!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est nemo aliquam sapiente blanditiis illo ducimus cumque, voluptate distinctio iusto magnam rem placeat, vero dicta dolorum sequi! Adipisci, fuga. Molestias, at!</p>
+      <div class="icons-container">
+        <div class="icons">
+          <i class="fas fa-user"></i>
+          <span>top destinations</span>
+        </div>
+        <div class="icons">
+          <i class="fas fa-hand-holding-usd"></i>
+          <span>affordable price</span>
+        </div>
+        <div class="icons">
+          <i class="fas fa-headset"></i>
+          <span>24/7 gride service</span>
+        </div>
+      </div>
+    </div>
+  </section>
+
   <!-- FOOTER -->
   <section class="footer">
     <div class="box-container">
@@ -81,6 +109,7 @@
 
     <div class="credit"><p>created by <span>mr. web design</span> | all rights reserved.</p></div>
   </section>
+
   <!-- swiper js file -->
   <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
   <!-- custon js file -->
