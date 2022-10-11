ddd96c809db6b3e7ada88c07d3b4f649d75e6f13
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-10 21:13:51 -0300

    home page

--- index.php
+++ index.php
@@ -38,6 +38,46 @@
   </section>
 
 
+  <!-- FOOTER -->
+  <section class="footer">
+    <div class="box-container">
+      
+      <div class="box">
+        <h3>quick links</h3>
+        <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask privacy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +123 456 789</a>
+        <a href="#"> <i class="fas fa-phone"></i> +123 456 789</a>
+        <a href="#"> <i class="fas fa-envelope"></i> contact@contact.com</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> recife, brazil, 99999</a>
+      </div>
+
+      <div class="box">
+        <h3>follow us</h3>
+        <a href=""><i class="fab fa-facebook-f"></i>facebook</a>
+        <a href=""><i class="fab fa-twitter"></i>twitter</a>
+        <a href=""><i class="fab fa-instagram"></i>instagram</a>
+        <a href=""><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+    </div>
+
+    <div class="credit"><p>created by <span>mr. web design</span> | all rights reserved.</p></div>
+  </section>
   <!-- swiper js file -->
   <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
   <!-- custon js file -->
