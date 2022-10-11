a726ad92ae70fd69bb1a297c8032a8ead25a6c0e
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-11 10:26:42 -0300

    services section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ menu.addEventListener('click', () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 })
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
